c25e630718238cc3651aac2f99d95a73b749c370
David <davbohr@gmail.com>
2016-05-27 17:35:09 +0200

    add /

--- gulpfile.js
+++ gulpfile.js
@@ -129,11 +129,11 @@ gulp.task('optimize-html-prod', ['optimize-js-prod'], function() {
   .pipe(minifyHTML({
     quotes: true
   }))
-		.pipe(replace(/<link rel=\"stylesheet\" href=\"public\/css\/all.min.css\"[^>]*>/, function(s) {
+		.pipe(replace(/<link rel=\"stylesheet\" href=\"\/public\/css\/all.min.css\"[^>]*>/, function(s) {
 			var style = fs.readFileSync('_site/public/css/all.min.css', 'utf8');
 			return '<style>' + style + '</style>';
 		}))
-		.pipe(replace(/<script src=\"public\/js\/all.min.js\"[^>]*><\/script>/, function(s) {
+		.pipe(replace(/<script src=\"\/public\/js\/all.min.js\"[^>]*><\/script>/, function(s) {
 			var js_script = fs.readFileSync('_site/public/js/all.min.js', 'utf8');
 			return '<script type="text/javascript">' + js_script + '</script>';
 		}))
